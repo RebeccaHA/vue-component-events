577c23a68208f23062bed2e6cf676a53edbda274
RebeccaHA <rebeccahunt034@gmail.com>
2021-01-19 21:17:11 +0000

    added two components and basic structure

---
+++ src/components/ActiveUser.vue
@@ -0,0 +1,10 @@
+<template>
+  <section>
+    <h2>{{name}}</h2>
+    <h3>{{age}}</h3>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
---
+++ src/components/UserData.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <input type="text" v-model="name" />
+    <input type="text" v-model="age" />
+    <button type="submit" @click="submitData">Submit</button>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      name: "",
+      age: null
+    };
+  },
+  methods: {
+    submitData() {
+      this.$emit("dataInput", name, age);
+    }
+  }
+};
+</script>
